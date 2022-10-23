35058049568b588c816eb767b86a0ff38fb190a6
Bas van den Wollenberg <hi@bas.sh>
2022-10-23 22:52:25 +0200

    Add putchar_fd, putendl_fd, putnbr_fd and putstr_fd

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_putnbr_fd.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: bvan-den <bvan-den@codam.nl>                 +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/23 22:52:04 by bvan-den      #+#    #+#                 */
+/*   Updated: 2022/10/23 22:52:05 by bvan-den      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <unistd.h>
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	unsigned int	num;
+
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		num = n * -1;
+	}
+	else
+		num = n;
+	if (num > 9)
+		ft_putnbr_fd(num / 10, fd);
+	ft_putchar_fd(num % 10 + '0', fd);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bvan-den <bvan-den@codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/04 14:24:09 by bvan-den      #+#    #+#                 */
-/*   Updated: 2022/10/23 21:58:06 by bvan-den      ########   odam.nl         */
+/*   Updated: 2022/10/23 22:30:23 by bvan-den      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,5 +50,9 @@ char	*ft_strndup(const char *s1, size_t len);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
