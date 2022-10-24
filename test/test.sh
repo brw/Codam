39bad83b89e114a5ca14c346eb4af5ac3f5e50a8
Bas van den Wollenberg <hi@bas.sh>
2022-10-24 22:28:36 +0200

    Remove old tests

--- test/test.sh
+++
@@ -1,6 +0,0 @@
-TEST_BIN=$1
-
-while IFS= read -r line
-do
-	./"$TEST_BIN" "$line"
-done < "$TEST_BIN"_inputs
--- test/test_ft_isalpha.c
+++
@@ -1,9 +0,0 @@
-#include "../libft.h"
-#include <ctype.h>
-#include <assert.h>
-
-int main(int argc, char *argv[])
-{
-	(void) argc;
-	assert(ft_isalpha(argv[1][0]) == isalpha(argv[1][0]));
-}
