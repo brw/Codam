c62cf7e196952a23086b864e9b896f2184c25f82
Bas van den Wollenberg <hi@bas.sh>
2022-10-28 22:28:05 +0200

    Fix content leak in lstmap

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,12 +6,22 @@
 /*   By: bvan-den <bvan-den@codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/27 20:15:22 by bvan-den      #+#    #+#                 */
-/*   Updated: 2022/10/27 20:22:09 by bvan-den      ########   odam.nl         */
+/*   Updated: 2022/10/28 22:06:44 by bvan-den      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
+
+t_list	*create_node(char *content, void (*del)(void *))
+{
+	t_list	*newlst;
+
+	newlst = ft_lstnew(content);
+	if (!newlst)
+		del(content);
+	return (newlst);
+}
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
@@ -19,23 +29,24 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*currentnode;
 	t_list	*newnode;
 
-	newlst = NULL;
-	if (lst)
+	if (!lst)
+		return (NULL);
+	currentnode = create_node(f(lst->content), del);
+	if (!currentnode)
+		return (NULL);
+	newlst = currentnode;
+	lst = lst->next;
+	while (lst)
 	{
-		newlst = ft_lstnew(f(lst->content));
-		if (!newlst)
-			return (NULL);
-		currentnode = newlst;
-		lst = lst->next;
-		while (lst)
+		newnode = create_node(f(lst->content), del);
+		if (!newnode)
 		{
-			newnode = ft_lstnew(f(lst->content));
-			if (!newnode)
-				return (ft_lstclear(&newlst, del), NULL);
-			currentnode->next = newnode;
-			currentnode = newnode;
-			lst = lst->next;
+			ft_lstclear(&newlst, del);
+			return (NULL);
 		}
+		currentnode->next = newnode;
+		currentnode = newnode;
+		lst = lst->next;
 	}
 	return (newlst);
 }
