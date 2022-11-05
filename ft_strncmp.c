6c270a9309d8db41cd4cbf81153377a81e975f91
Bas van den Wollenberg <hi@bas.sh>
2022-11-05 18:00:10 +0100

    Remove unnecessar null terminator check

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: bvan-den <bvan-den@student.codam.nl>        +#+                      */
 /*                                                  +#+                       */
 /*   Created: 2022/10/30 18:53:50 by bvan-den      #+#    #+#                 */
-/*   Updated: 2022/10/30 18:53:50 by bvan-den      ########   odam.nl         */
+/*   Updated: 2022/11/02 22:35:32 by bvan-den      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	if (n == 0)
 		return (0);
 	i = 0;
-	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n - 1)
+	while (s1[i] && s1[i] == s2[i] && i < n - 1)
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
