ca186268364c1adbf971800c819dda8c37f1687f
Bas van den Wollenberg <hi@bas.sh>
2022-11-13 21:32:28 +0100

    Fix include guard

--- ft_printf/include/ft_printf.h
+++ ft_printf/include/ft_printf.h
@@ -6,12 +6,12 @@
 /*   By: bvan-den <bvan-den@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/09 06:14:00 by bvan-den      #+#    #+#                 */
-/*   Updated: 2022/11/10 20:25:17 by bvan-den      ########   odam.nl         */
+/*   Updated: 2022/11/13 21:30:06 by bvan-den      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
-# define LIBFT_H
+# define FT_PRINTF_H
 
 # include <stdarg.h>
 
