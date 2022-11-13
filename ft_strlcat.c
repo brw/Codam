0cdbef653ef275400af6d61f9ab2c6a3519e0ebb
Bas van den Wollenberg <hi@bas.sh>
2022-11-13 01:10:18 +0100

    Add include path when compiling

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "include/libft.h"
+#include <libft.h>
 #include <stddef.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
