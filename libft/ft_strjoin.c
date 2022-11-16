5c822375ecd08be84e73d7cf9085b60bd0ceb38f
Bas van den Wollenberg <hi@bas.sh>
2022-11-16 19:28:35 +0100

    Change strlcpy to memcpy

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: bvan-den <bvan-den@student.codam.nl>        +#+                      */
 /*                                                  +#+                       */
 /*   Created: 2022/10/30 18:53:50 by bvan-den      #+#    #+#                 */
-/*   Updated: 2022/10/30 20:39:22 by bvan-den      ########   odam.nl         */
+/*   Updated: 2022/11/16 19:24:33 by bvan-den      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	str = malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (!str)
 		return (NULL);
-	ft_strlcpy(str, s1, s1_len + 1);
+	ft_memcpy(str, s1, s1_len + 1);
 	ft_strlcat(str, s2, s1_len + s2_len + 1);
 	return (str);
 }
