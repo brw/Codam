ed0ffe4a42c2d4f6f54616db8c9e9f70b681a29a
Bas van den Wollenberg <hi@bas.sh>
2022-12-02 12:28:53 +0100

    Remove non-existing function from header file

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -8,7 +8,6 @@ size_t	ft_strlen(const char *s);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-char	*ft_strndup(const char *s1, size_t len);
 char	*ft_strchr(const char *s, int c);
 
 #endif
