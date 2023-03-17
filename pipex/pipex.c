8f7824e7f05329a19b177f8a53d15418707190a3
Bas van den Wollenberg <hi@bas.sh>
2023-03-17 18:20:17 +0100

    Add path separator

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -20,7 +20,7 @@ char	*get_cmd_path(char *cmd, char **paths)
 
 	while (*paths)
 	{
-		try_path = ft_strjoin(*paths, cmd);
+		try_path = ft_strjoin(ft_strjoin(*paths, "/"), cmd);
 		if (access(try_path, X_OK) == 0)
 			return (try_path);
 		free(try_path);
@@ -38,13 +38,11 @@ int	main(int argc, char **argv, char **envp)
 	char	**paths;
 
 	infile = argv[1];
-	cmd1 = argv[2];
+	paths = ft_split(find_path_env(envp) + 5, ':');
+	cmd1 = get_cmd_path(argv[2], paths);
 	char	*args[] = { "-l", NULL };
 	cmd2 = argv[3];
 	outfile = argv[4];
 
-	paths = ft_split(find_path_env(envp) + 5, ':');
-	printf("%s\n", get_cmd_path(cmd1, paths));
-
-	execve("ls", args, envp);
+	execve(cmd1, args, envp);
 }
