89efe946a10402db8ba914a7f96caf1419c2da01
Bas van den Wollenberg <hi@bas.sh>
2023-06-13 22:17:55 +0200

    Attempt to handle empty strings/leading spaces as best as I can
    Only thing I'm not handling is spaces after the command but that seems
    a bit out of the scope for this project since it's already difficult to
    handle quotes when quotes are already used to give commands with
    arguments.

--- libft/ft_split_args.c
+++ libft/ft_split_args.c
@@ -6,14 +6,14 @@
 /*   By: bvan-den <bvan-den@student.codam.nl>        +#+                      */
 /*                                                  +#+                       */
 /*   Created: 2022/10/30 18:53:49 by bvan-den      #+#    #+#                 */
-/*   Updated: 2023/05/31 22:33:20 by bvan-den      ########   odam.nl         */
+/*   Updated: 2023/06/13 22:14:05 by bvan-den      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 #include <stddef.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 static size_t	count_items(char const *s, char delim)
 {
@@ -39,11 +39,14 @@ static size_t	count_items(char const *s, char delim)
 	return (count);
 }
 
-static size_t	get_item_length(const char **s, char delim)
+static size_t	get_item_length(const char **s, char delim, size_t index)
 {
 	size_t	i;
 
 	i = 0;
+	if (index == 0)
+		while ((*s)[i] == ' ')
+			i++;
 	if (**s == '\'' || **s == '"')
 	{
 		i++;
@@ -53,12 +56,10 @@ static size_t	get_item_length(const char **s, char delim)
 			i++;
 	}
 	else
-	{
 		while ((*s)[i] && ((*s)[i] != delim || (*s)[i - 1] == '\\'))
 			i++;
-	}
-	if (((*s)[i - 1] == '\'' && **s == '\'') ||
-		((*s)[i - 1] == '"' && **s == '"'))
+	if ((i > 0 && (*s)[i - 1] == '\'' && **s == '\'') ||
+		(i > 0 && (*s)[i - 1] == '"' && **s == '"'))
 	{
 		(*s)++;
 		i -= 2;
@@ -90,9 +91,7 @@ char	**ft_split_args(char const *str)
 	index = 0;
 	while (*str && index < count)
 	{
-		while (*str == ' ')
-			str++;
-		length = get_item_length(&str, ' ');
+		length = get_item_length(&str, ' ', index);
 		arr[index] = ft_strndup(str, length);
 		if (!arr[index])
 			return (free_arr(arr, index), NULL);
